6a5fa6f82dd2197fb113172f2b357e03e5a81e25
Aaron M. Griffin <uurrnn@gmail.com>
2017-10-20 15:14:48 -0400

    updating per tutorial

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Javascripts: The Good Parts' },
+        { title: 'Harry Potter' },
+        { title: 'The Dark Tower' },
+        { title: 'Eloquent Ruby' }
+    ]
+}
